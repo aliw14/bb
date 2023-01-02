fe3c6c20d90e74829778f736c4fcefb8824c5bf6
ali <ali@MacBook-Pro-Ali.local>
2023-01-03 01:57:58 +0400

    mesaj

---
+++ bb/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace bb;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        int count = 0;
+        
+        int[] numbers = { 3, 5, 6, 2, 7, 8 };
+        for (int i = 1; i < numbers.Length; i+=2)
+
+        {
+
+
+
+            if (numbers[i] % 2 == 0)
+            {
+                count++;
+                sum += numbers[i];
+            }
+
+            }Console.WriteLine(sum + " " + count);
+
+       
+        
+
+        
+
+
+    }
+}
+
